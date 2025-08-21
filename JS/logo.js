801a58145d5beaae645ab7b5c096fb2dcfde473d
inmaria722 <tncrkak722@naver.com>
2025-08-21 14:01:45 +0900

    js, logo 추가

---
+++ JS/logo.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.fromTo(
+    ".symbol",
+    {
+      scale: 0,
+      opacity: 0,
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: ".logo-box",
+        start: "top bottom", // 로고박스가 화면 중간에 올 때 시작
+        toggleActions: "play none none none",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    ".typography",
+    {
+      scale: 0,
+      opacity: 0,
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      delay: 0.2,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: ".logo-box",
+        start: "top center",
+        toggleActions: "play none none none",
+      },
+    }
+  );
+});
