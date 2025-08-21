24a1395c19c679a1271946d982bbceebdcde1735
inmaria722 <tncrkak722@naver.com>
2025-08-22 01:07:03 +0900

    js, routine 추가

---
+++ JS/routine.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  //공동 속성
+  $('[fade="up"]').each(function (i, e) {
+    const delay = $(e).data("delay") || 0;
+
+    gsap.from(e, {
+      opacity: 0,
+      y: 60,
+      duration: 0.8,
+      delay: delay,
+      scrollTrigger: {
+        trigger: e,
+        start: "top 90%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
+
+  $('[fade="right"]').each(function (i, e) {
+    const delay = $(e).data("delay") || 0;
+
+    gsap.from(e, {
+      opacity: 0,
+      x: -60,
+      duration: 1,
+      delay: delay,
+      scrollTrigger: {
+        trigger: e,
+        start: "top 90%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
+});
