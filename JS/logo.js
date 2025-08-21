0838de5c0be3857ad8495b8984020de384243bff
inmaria722 <tncrkak722@naver.com>
2025-08-21 14:15:21 +0900

    js, logo 추가

--- JS/logo.js
+++ JS/logo.js
@@ -39,4 +39,44 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     }
   );
+
+  gsap.fromTo(
+    ".lines-v",
+    {
+      scale: 2,
+      opacity: 0,
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      delay: 0.2,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: ".logo-box",
+        start: "top center",
+        toggleActions: "play none none none",
+      },
+    }
+  );
+
+  gsap.fromTo(
+    ".lines-h",
+    {
+      scale: 1.7,
+      opacity: 0,
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 1,
+      delay: 0.4,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: ".logo-box",
+        start: "top center",
+        toggleActions: "play none none none",
+      },
+    }
+  );
 });
