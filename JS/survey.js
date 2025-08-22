f440680eccec00fd05d8bfe32c73035b32d46c1e
Sim-yuna <simpson.16na@gmail.com>
2025-08-22 15:52:12 +0900

    survey JS 수정

--- JS/survey.js
+++ JS/survey.js
@@ -43,17 +43,21 @@ window.addEventListener("DOMContentLoaded", function () {
     scrub: 1,
   });
   surveyBox1
-    .to(".survey-con-box-1", { clipPath: "inset(0% 0% 0% 0%)" }, 0)
-    .to(".survey-con-box-1 .circle-graph", { scale: 1 }, 0)
+    .to(".survey-box .survey-con-box-1", { clipPath: "inset(0% 0% 0% 0%)" }, 0)
+    .to(".survey-box .survey-con-box-1 .circle-graph", { scale: 1 }, 0)
     .to(
-      ".survey-con-box-1 .graph-line",
+      ".survey-box .survey-con-box-1 .graph-line",
       { clipPath: "inset(0% 0% 0% 0%)", opacity: 1 },
       0.3
     )
-    .to(".survey-con-box-1 .bar-bar", { scaleX: 1 }, 0.3)
-    .to(".survey-con-box-1 .bar-bar, .bar-line", { scaleX: 1 }, 0.5)
-    .to(".survey-con-box-1 .bar-bar, .bar-line", { scaleX: 1 }, 0.6)
-    .to(".survey-con-box-1 .bar-bar, .bar-line", { scaleX: 1 }, 0.7);
+    .to(".survey-box .survey-con-box-1 .bar-bar", { scaleX: 1 }, 0.3)
+    .to(".survey-box .survey-con-box-1 .bar-bar, .bar-line", { scaleX: 1 }, 0.5)
+    .to(".survey-box .survey-con-box-1 .bar-bar, .bar-line", { scaleX: 1 }, 0.6)
+    .to(
+      ".survey-box .survey-con-box-1 .bar-bar, .bar-line",
+      { scaleX: 1 },
+      0.7
+    );
 
   const surveyBox2 = gsap.timeline();
   ScrollTrigger.create({
@@ -64,13 +68,13 @@ window.addEventListener("DOMContentLoaded", function () {
     scrub: 1,
   });
   surveyBox2
-    .to(".survey-con-box-2", { clipPath: "inset(0% 0% 0% 0%)" }, 0)
-    .to(".survey-con-box-2 .bar-col-1", { scaleY: 1 }, 0)
-    .to(".survey-con-box-2 .bar-col-2", { scaleY: 1 }, 0.2)
+    .to(".survey-box .survey-con-box-2", { clipPath: "inset(0% 0% 0% 0%)" }, 0)
+    .to(".survey-box .survey-con-box-2 .bar-col-1", { scaleY: 1 }, 0)
+    .to(".survey-box .survey-con-box-2 .bar-col-2", { scaleY: 1 }, 0.2)
     .to(
-      ".survey-con-box-2 .bar-col-line",
+      ".survey-box .survey-con-box-2 .bar-col-line",
       { clipPath: "inset(0% 0% 0% 0%)", opacity: 1 },
       0.6
     )
-    .to(".survey-con-box-2 .right-box-con", { scale: 1 }, 0.7);
+    .to(".survey-box .survey-con-box-2 .right-box-con", { scale: 1 }, 0.7);
 });
