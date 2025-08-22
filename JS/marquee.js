abfae5b6850d34c837289bf27867b4111152c0a7
Sim_yuna <simpson.16na@gmail.com>
2025-08-22 09:08:08 +0900

    marquee js 추가

---
+++ JS/marquee.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  //marquee
+  const $wrap = $(".marquee-wrap");
+  const $box = $(".marquee-box");
+  const originalContent = $wrap.html();
+
+  while ($wrap[0].scrollWidth < $box.width() * 3) {
+    $wrap.append(originalContent);
+  }
+});
