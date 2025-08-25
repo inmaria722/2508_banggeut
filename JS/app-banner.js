481590bf319572475e7079b72769aad7223d254c
Sim_yuna <simpson.16na@gmail.com>
2025-08-25 12:28:49 +0900

    미나 부분 이미지 변경

--- JS/app-banner.js
+++ JS/app-banner.js
@@ -1,23 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
 
-  //공동 속성
-  $('[fade="up"]').each(function (i, e) {
-    const delay = $(e).data("delay") || 0;
-
-    gsap.from(e, {
-      opacity: 0,
-      y: 60,
-      duration: 0.8,
-      delay: delay,
-      scrollTrigger: {
-        trigger: e,
-        start: "top 90%",
-        toggleActions: "play none none reverse",
-      },
-    });
-  });
-
   $('[slide="right"]').each(function (i, e) {
     const delay = $(e).data("delay") || 0;
 
