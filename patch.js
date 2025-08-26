186606c59a8761d7e4951e2fcea126d1910de88d
inmaria722 <tncrkak722@naver.com>
2025-08-26 15:23:05 +0900

    Add files via upload

---
+++ patch.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  $(".patch-box").each(function (i, box) {
+    const patch = $(box).find(".patch")[0];
+    const delay = $(patch).data("delay") || 0;
+
+    ScrollTrigger.create({
+      trigger: box,
+      start: "top 90%",
+      onEnter: () => {
+        gsap.to(patch, {
+          opacity: 1,
+          scale: 1,
+          delay: delay,
+          duration: 0.6,
+          ease: "circ.in",
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(patch, {
+          opacity: 0,
+          scale: 1.4,
+          duration: 0.6,
+          ease: "power2.in",
+        });
+      },
+    });
+  });
+});
